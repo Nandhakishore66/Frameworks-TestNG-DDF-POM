c4d77014e6d406a5fe0723578d5891315b684841
Nandhakishore66 <nandhakishore.ravi66@gmail.com>
2025-03-08 14:28:11 +0530

    Update TestNGPart12Impl.java

--- src/com/TestNG/TestNGPart12Impl.java
+++ src/com/TestNG/TestNGPart12Impl.java
@@ -3,8 +3,43 @@ package com.TestNG;
 import org.testng.ITestResult;
 
 public class TestNGPart12Impl extends TestNGPart12 {
-    @Override
+   @Override
     public void onTestStart(ITestResult result) {
         super.onTestStart(result);
+    }
+
+    @Override
+    public void onTestSuccess(ITestResult result) {
+        super.onTestSuccess(result);
+    }
+
+    @Override
+    public void onTestFailure(ITestResult result) {
+        super.onTestFailure(result);
+    }
+
+    @Override
+    public void onTestSkipped(ITestResult result) {
+        super.onTestSkipped(result);
+    }
+
+    @Override
+    public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
+        super.onTestFailedButWithinSuccessPercentage(result);
+    }
+
+    @Override
+    public void onTestFailedWithTimeout(ITestResult result) {
+        super.onTestFailedWithTimeout(result);
+    }
+
+    @Override
+    public void onStart(ITestContext context) {
+        super.onStart(context);
+    }
+
+    @Override
+    public void onFinish(ITestContext context) {
+        super.onFinish(context);
     }
 }
