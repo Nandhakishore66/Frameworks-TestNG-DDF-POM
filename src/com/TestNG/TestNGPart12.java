0664f22640f9c4617245ea9ef3787171e93e07c0
Nandhakishore66 <nandhakishore.ravi66@gmail.com>
2025-04-08 17:45:43 +0530

    Update TestNGPart12.java

--- src/com/TestNG/TestNGPart12.java
+++ src/com/TestNG/TestNGPart12.java
@@ -4,5 +4,11 @@ import org.testng.ITestListener;
 
 public class TestNGPart12 implements ITestListener {
 
+  
+    //Steps to implement unimplemented methods from the ITestlistners
+    //1. hover on the class name
+    //2. Click more actions
+    //3. create sub class and use control click to select all methods and click ok.
+
 
 }
