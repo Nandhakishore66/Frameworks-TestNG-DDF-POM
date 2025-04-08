a2df51a2af5ef94f31a1ae460734183a7dc9f551
Nandhakishore66 <nandhakishore.ravi66@gmail.com>
2025-04-08 17:46:46 +0530

    Update TestNGPart12Impl.java

--- src/com/TestNG/TestNGPart12Impl.java
+++ src/com/TestNG/TestNGPart12Impl.java
@@ -3,43 +3,52 @@ package com.TestNG;
 import org.testng.ITestResult;
 
 public class TestNGPart12Impl extends TestNGPart12 {
-   @Override
+//As we are going to override the functionality for the methods, it is having override at the top.
+
+    //one more point..
+    /*
+    Here I have added only print statement for understanding purpose but in realtime it will be
+    like, Suppose if the testcase failed suddenly it will go for onTestFailure listener, then
+    if we have written some screenshot functionality then it will be very useful right, like that
+    we can design a very good framework....
+     */
+    @Override
     public void onTestStart(ITestResult result) {
-        super.onTestStart(result);
+        System.out.println("Test going to execute");
     }
 
     @Override
     public void onTestSuccess(ITestResult result) {
-        super.onTestSuccess(result);
+        System.out.println("Test succeed");
     }
 
     @Override
     public void onTestFailure(ITestResult result) {
-        super.onTestFailure(result);
+        System.out.println("Test failed");
     }
 
     @Override
     public void onTestSkipped(ITestResult result) {
-        super.onTestSkipped(result);
+        System.out.println("Test skipped");
     }
 
     @Override
     public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
-        super.onTestFailedButWithinSuccessPercentage(result);
+        System.out.println("Test percentage");
     }
 
     @Override
     public void onTestFailedWithTimeout(ITestResult result) {
-        super.onTestFailedWithTimeout(result);
+        System.out.println("Test failed because of timeout");
     }
 
     @Override
     public void onStart(ITestContext context) {
-        super.onStart(context);
+        System.out.println("Test going to start");
     }
 
     @Override
     public void onFinish(ITestContext context) {
-        super.onFinish(context);
+        System.out.println("Test going to finish");
     }
 }
